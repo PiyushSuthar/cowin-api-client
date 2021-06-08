c940fed2829fae51bb045a1b062b8441d7a4524c
Piyush Suthar <32237558+PiyushSuthar@users.noreply.github.com>
2021-06-08 19:59:55 +0530

    :ambulance: Fixed: CORS issue dut to `user-agent` header

--- src/CONSTANTS/API.ts
+++ src/CONSTANTS/API.ts
@@ -4,7 +4,7 @@ export const API_URL = "https://cdn-api.co-vin.in/api"
 
 export const API = axios.create({
   baseURL: API_URL,
-  headers: {
+  headers: typeof Window === "undefined" && {
     // To make it work in React Native :)
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
   }
